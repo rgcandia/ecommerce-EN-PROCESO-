62391d9bd8dd11e65433d6f42a2e4ab705275721
Roque Alejandro Gonzalez Candia <alejandrogcandia@gmail.com>
2023-04-30 11:41:09 -0300

    primer commit

---
+++ src/redux/slice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+    items:null
+}
+export const itemsSlice = createSlice({
+    name:'items',
+    initialState,
+    reducers:{
+        setItems:(state,action)=>{
+            state.items= action.payload
+        }
+    }
+})
+
+export const {setItems} =  itemsSlice.actions;
+export default itemsSlice.reducer;
