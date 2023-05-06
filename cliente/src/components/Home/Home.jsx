cc6cf60eb9d160e9195e346dab0a104d217c04ba
Roque Alejandro Gonzalez Candia <alejandrogcandia@gmail.com>
2023-05-06 08:19:33 -0300

    Se configura componente Detail, se verifica el correcto funcionamiento, falta retocar

--- cliente/src/components/Home/Home.jsx
+++ cliente/src/components/Home/Home.jsx
@@ -1,11 +1,14 @@
 import Nav from '../Nav/Nav.jsx';
 import View from '../View/View.jsx';
+import Detail from '../Detail/Detail.jsx'
 import Footer from '../Footer/Footer.jsx';
 import style from './Home.module.css'
+import { useSelector } from 'react-redux';
 const Home = ()=>{
+    const {selected} = useSelector(state=>state.dataBase)
     return(<div className={style.home}>
         <Nav/>
-        <View/>
+        {selected?<Detail/>:<View/>}
         <Footer/>
     </div>)
 }
